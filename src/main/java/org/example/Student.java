dbdd1a2590e73536a16625e72e2285313fe587ef
Thivya15 <thivyamahendran@gmail.com>
2025-01-25 13:32:52 +0530

    commit2

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -25,40 +25,40 @@ public class Student {
             int res = preparedStatement.executeUpdate(); // can use  preparedStatement.executeUpdate();
 
             if (res > 0) {
-                return "Successfully added the record of the employee Id: " + id;
+                return "Successfully added the record of the student Id: " + id;
             } else {
-                return "Registering employee is unsuccessful!";
+                return "Registering student is unsuccessful!";
             }
         } catch (SQLException e) {
-            return "Registering employee is unsuccessful! Reason: " + e.getMessage();
+            return "Registering student is unsuccessful! Reason: " + e.getMessage();
         }
     }
 
     public String viewStudent(String id, Connection conn){
-        String sql = "SELECT * FROM employees WHERE employee_id = ?;";
+        String sql = "SELECT * FROM student WHERE student_id = ?;";
         StringBuilder result = new StringBuilder();
         try(PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
             preparedStatement.setString(1,id);
             ResultSet res = preparedStatement.executeQuery();
 
             if (res.next()){
-                String empId = res.getString("employee_id");
+                String empId = res.getString("student_id");
                 String name = res.getString("name");
                 int age = res.getInt("age");
                 String department = res.getString("department");
                 return String.format("ID: %s, Name: %s, Age: %d, Department: %s\n", id, name, age, department);
 
             } else {
-                return "employee records are empty!";
+                return "student records are empty!";
             }
         }
         catch (SQLException e){
-            return "Retrieving employee is unsuccessful! Reason: "+ e.getSQLState();
+            return "Retrieving student is unsuccessful! Reason: "+ e.getSQLState();
         }
     }
 
     public String updateStudent(String id, String attribute, Object value,Connection conn){
-        String sql = "UPDATE employees SET " + attribute + " = ? WHERE employee_id = ?";
+        String sql = "UPDATE student SET " + attribute + " = ? WHERE student_id = ?";
         try(PreparedStatement preparedStatement = conn.prepareStatement(sql);) {
 
             if(!attribute.equals("age")) {
@@ -70,35 +70,35 @@ public class Student {
 
             int res = preparedStatement.executeUpdate(); // can use  preparedStatement.executeUpdate();
             if (res>0) {
-                return "Successfully updated the record of the employee Id: " + id;
+                return "Successfully updated the record of the student Id: " + id;
             } else {
-                return "Updating employee is unsuccessful!";
+                return "Updating student is unsuccessful!";
             }
         }
         catch (SQLException e){
-            return "Updating employee is unsuccessful! Reason: "+ e.getMessage();
+            return "Updating student is unsuccessful! Reason: "+ e.getMessage();
         }
     }
 
     public String deleteStudent(String id, Connection conn){
-        String sql = "DELETE FROM employees WHERE employee_id = ?";
+        String sql = "DELETE FROM student WHERE student_id = ?";
         try(PreparedStatement preparedStatement = conn.prepareStatement(sql);) {
 
             preparedStatement.setString(1,id);
 
             int row = preparedStatement.executeUpdate();
             if (row > 0) {
-                return "Successfully deleted the record of the employee Id: " + id;
+                return "Successfully deleted the record of the student Id: " + id;
             }
             else if (row == 0) {
-                return "Given employee is not found!";
+                return "Given student is not found!";
             }
             else{
-                return "Deleting employee is unsuccessful!";
+                return "Deleting student is unsuccessful!";
             }
         }
         catch (SQLException e){
-            return "Deleting employee is unsuccessful! Reason: "+ e.getMessage();
+            return "Deleting student is unsuccessful! Reason: "+ e.getMessage();
         }
     }
 
